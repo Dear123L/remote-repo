03d1a315e2168f437385048b8d6f8817af4a4f81
Dear123L <2630420236@qq.com>
2025-03-28 21:43:33 +0800

    第三次提交 ch01-ch03

--- src/main/java/com/learn/mapper/IUserMapper.java
+++ src/main/java/com/learn/mapper/IUserMapper.java
@@ -1,6 +1,8 @@
 package com.learn.mapper;
 
 import com.learn.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -8,13 +10,7 @@ import java.util.List;
  * @author lhh
  */
 public interface IUserMapper {
-    List<User> getAllUsers();
-
-    User getUserById(Integer id);
-
-    int insertUser(User user);
-
-    int updateUser(User user);
-
-    int deleteUserById(Integer id);
+    @Select("select * from learn_mybatis where name = #{name} And password = #{password}")
+        //当方法存在多个参数时，参数前必须加上@Param("name")注解
+    User getUserByNameAndPass(@Param("name") String name, @Param("password") String password);
 }
--- src/test/java/com/learn/mapper/UserMapperTest.java
+++ src/test/java/com/learn/mapper/UserMapperTest.java
@@ -12,15 +12,61 @@ import java.util.List;
  */
 public class UserMapperTest {
     @Test
-    public void testSelectAll(){
-        SqlSession sqlSession= MybatisUtil.getSqlSession();
-//        这个mapper对象就是UserMapper的实现类
-//        getMapper是获取对应接口的实现类，用的时候就传入对应接口的class对象
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-//        我们可以通过这个实现类来操作数据库
+    public void testGetAllUsers(){
+        // 先通过刚才写的工具类获取会话对象
+        // Session就是会话，SqlSession就是指程序和数据库的会话，就是程序和数据库的连接
+        SqlSession session = MybatisUtil.getSqlSession();
+        // 使用会话对象的getMapper()方法可以获取mybatis动态生成的实现类
+        UserMapper mapper = session.getMapper(UserMapper.class);
         List<User> allUsers = mapper.getAllUsers();
-        for(User user:allUsers){
-            System.out.println(user);
-        }
+        System.out.println(allUsers);
+        session.close();
+    }
+    @Test
+    public void testGetUserById(){
+        SqlSession session = MybatisUtil.getSqlSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.getUserById(1);
+        System.out.println(user);
+        session.close();
+    }
+    @Test
+    public void testInsertUser(){
+        SqlSession session = MybatisUtil.getSqlSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        int result = mapper.insertUser(new User(null, "李四", "123456"));
+        System.out.println(result);
+        session.commit();
+        session.close();
+    }
+    @Test
+    public void testUpdateUser(){
+        SqlSession session = MybatisUtil.getSqlSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User();
+        user.setId(1);
+        user.setPassword("789");
+        int count = mapper.updateUser(user);
+        System.out.println(count);
+        session.commit();
+        session.close();
+    }
+    @Test
+    public void testDeleteUserById() {
+        SqlSession session = MybatisUtil.getSqlSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        int result = mapper.deleteUserById(12);
+        System.out.println(result);
+        session.commit();
+        session.close();
+    }
+
+    @Test
+    public void testGetUserByNameAndPass(){
+        SqlSession session = MybatisUtil.getSqlSession();
+        IUserMapper mapper = session.getMapper(IUserMapper.class);
+        User user = mapper.getUserByNameAndPass("hh","123456");
+        System.out.println(user);
+        session.close();
     }
 }
